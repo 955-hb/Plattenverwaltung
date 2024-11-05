52a8afc27b3242935cdf8d818cf68c5d613eb29b
Hannes Bröhl <hannes.broehl@icloud.com>
2024-11-05 14:10:29 +0100

    Dateiname mit richtiger Uhrzeit

--- script.js
+++ script.js
@@ -66,13 +66,13 @@ document.addEventListener("DOMContentLoaded", function () {
       };
       return new Intl.DateTimeFormat("de-DE", options)
         .format(date)
-        .replace(/\./g, "-")
+        .replace(/\./g, "/")
         .replace(",", "");
     };
 
     // generiere aktuellen Zeitstempel +1Hour
     const currentDate = new Date();
-    currentDate.setHours(currentDate.getHours() + 1);
+    currentDate.setHours(currentDate.getHours());
     const timeStamp = formatDate(currentDate);
 
     // #1 Arbeitsblatt aus Array erstellen
